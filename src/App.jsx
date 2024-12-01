13e5ee42c152923c3a4eba1e2cc1d6ba1a057577
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-12-01 19:54:08 +0200

    handle app.jsx responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
-import './App.css'
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import LandingPage from './pages/LandingPage'
-import Join from './components/Join'
+import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import LandingPage from './pages/LandingPage';
+import Join from './components/Join';
 
 function App() {
-    
-
     return (
         <>
             <Navbar />
             <Hero />
-            <div className='pl-52 bg-gray-100'>
-                
+            <div className="bg-gray-100 pl-0 md:pl-52">
                 <LandingPage />
-                
                 <Join />
             </div>
             <Footer />
-            
         </>
-    )
+    );
 }
 
-export default App
+export default App;
