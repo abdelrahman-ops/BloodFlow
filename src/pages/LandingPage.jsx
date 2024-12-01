800a1b536a7cf59a56a581c3c563d2e3d6446a45
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-12-01 19:58:20 +0200

    handle responsive , add icon and some styles

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,66 +1,74 @@
+import { motion } from 'framer-motion';
+import { AiOutlineEye } from 'react-icons/ai'; // Importing the open icon from React Icons
+import inventory from '../assets/inventory.jpg';
+import support from '../assets/support.jpg';
+import doctors from '../assets/doctors.jpg';
+import donor from '../assets/donor.jpg';
 
-import inventory from '../assets/inventory.jpg'
-import support from '../assets/support.jpg'
-import doctors from '../assets/doctors.jpg'
-import donor from '../assets/donor.jpg'
 function LandingPage() {
     return (
         <div className="min-h-screen">
-            <section id="features" className="py-16 px-8 bg-gray-100">
-                <h3 className="text-3xl font-bold text-center mb-8">Why Choose BloodFlow?</h3>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-                    
-                    {/* First card with top-left border radius */}
-                    <div className="bg-white shadow-md p-6 rounded-b-full rounded-tl-[1500px] rounded-tr-full flex flex-col justify-between overflow-hidden">
-                        <div>
-                            <h4 className="text-xl font-bold mb-4">Real-Time Blood Inventory</h4>
-                            <p className="mb-4">Track available blood types and ensure timely distribution during emergencies.</p>
-                        </div>
-                        <img 
-                            src={inventory}
-                            alt="Real-Time Blood Inventory"
-                            className="w-48 h-48  rounded-full mx-auto"
-                        />
-                    </div>
-                    
-                    {/* Second card */}
-                    <div className="bg-white shadow-md p-6 rounded-b-full rounded-tl-[1500px] rounded-tr-full flex flex-col justify-between overflow-hidden">
-                        <div>
-                            <h4 className="text-xl font-bold mb-4">Integrated with Hospitals</h4>
-                            <p className="mb-4">Seamlessly connect with healthcare centers for efficient blood requests and delivery.</p>
-                        </div>
-                        <img 
-                            src={doctors}
-                            alt="Integrated with Hospitals"
-                            className="w-48 h-48 rounded-full mx-auto"
-                        />
-                    </div>
-                    
-                    {/* Third card */}
-                    <div className="bg-white shadow-md p-6 rounded-b-full rounded-tl-[1500px] rounded-tr-full flex flex-col justify-between overflow-hidden">
-                        <div>
-                            <h4 className="text-xl font-bold mb-4">Engage Donors Easily</h4>
-                            <p className="mb-4">Use advanced technology to remind and engage donors through mobile apps and notifications.</p>
-                        </div>
-                        <img 
-                            src={donor}
-                            alt="Engage Donors Easily"
-                            className="w-48 h-48 rounded-full mx-auto"
-                        />
-                    </div>
+            <section id="features" className="py-16 px-4 sm:px-8 bg-gray-100">
+                <h3 className="text-3xl font-bold text-center mb-12">
+                    Why Choose BloodFlow?
+                </h3>
+                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+                    {/* Card Components */}
+                    {[
+                        {
+                            title: 'Real-Time Blood Inventory',
+                            description: 'Track available blood types and ensure timely distribution during emergencies.',
+                            imgSrc: inventory,
+                            altText: 'Real-Time Blood Inventory',
+                        },
+                        {
+                            title: 'Integrated with Hospitals',
+                            description: 'Seamlessly connect with healthcare centers for efficient blood requests and delivery.',
+                            imgSrc: doctors,
+                            altText: 'Integrated with Hospitals',
+                        },
+                        {
+                            title: 'Engage Donors Easily',
+                            description: 'Use advanced technology to remind and engage donors through mobile apps and notifications.',
+                            imgSrc: donor,
+                            altText: 'Engage Donors Easily',
+                        },
+                        {
+                            title: 'Community Support',
+                            description: 'Join a network of donors and recipients to build a supportive community.',
+                            imgSrc: support,
+                            altText: 'Community Support',
+                        },
+                    ].map((card, index) => (
+                        <motion.div
+                            key={index}
+                            initial={{ opacity: 0, y: 20 }}
+                            whileInView={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 0.5 }}
+                            className="bg-white shadow-md p-4 sm:p-6 rounded-b-full rounded-tl-[1500px] 
+                            rounded-tr-full flex flex-col justify-between items-center overflow-hidden transition-transform 
+                            hover:scale-105 duration-300 w-full sm:w-[95%] md:w-[85%] lg:w-full mx-auto relative hover:bg-red-500 hover:bg-opacity-30"
+                        >
+                            <h4 className="text-lg sm:text-xl font-bold mb-4 text-center">
+                                {card.title}
+                            </h4>
+                            <p className="mb-6 text-sm sm:text-base text-center">
+                                {card.description}
+                            </p>
 
-                    {/* Fourth card */}
-                    <div className="bg-white shadow-md p-6 rounded-b-full rounded-tl-[1500px] rounded-tr-full flex flex-col justify-between overflow-hidden">
-                        <div>
-                            <h4 className="text-xl font-bold mb-4">Community<br></br>Support</h4>
-                            <p className="mb-4">Join a network of donors and recipients to build a supportive community.</p>
-                        </div>
-                        <img 
-                            src={support}
-                            alt="Community Support"
-                            className="w-48 h-48 rounded-full mx-auto"
-                        />
-                    </div>
+                            {/* Image container with the icon */}
+                            <div className="relative w-full flex justify-center items-center">
+                                <img
+                                    src={card.imgSrc}
+                                    alt={card.altText}
+                                    className="w-24 h-24 sm:w-32 sm:h-32 lg:w-40 lg:h-40 rounded-full hover:scale-110"
+                                />
+                                <div className="absolute top-1 right-1 flex items-center justify-center w-8 h-8 bg-red-800 text-white rounded-full shadow-lg hover:bg-red-600 transition duration-300">
+                                    <AiOutlineEye className="w-4 h-4" />
+                                </div>
+                            </div>
+                        </motion.div>
+                    ))}
                 </div>
             </section>
         </div>
