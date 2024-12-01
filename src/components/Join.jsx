f25e3560806ccc5316ccafb70aaff07f73c5737c
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-12-01 19:56:31 +0200

    handle styles

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -1,14 +1,17 @@
 const Join = () => {
     return (
-        <section className="relative bg-red-500 text-white text-center py-12 px-8">
-            <h3 className="text-3xl font-bold">Become a Lifesaver Today!</h3>
-            <p className="mt-4">Sign up as a donor or make a request for blood in just a few clicks.</p>
-            <button className="mt-6 bg-white text-red-600 px-6 py-3 rounded font-semibold hover:bg-gray-200">
+        <section className="relative bg-gradient-to-r from-red-600 to-red-900 text-white text-center py-12 px-6 sm:px-8 rounded-tl-full shadow-lg">
+            <h3 className="text-3xl sm:text-4xl font-bold mb-4">
+                Become a Lifesaver Today!
+            </h3>
+            <p className="mt-4 text-sm sm:text-base">
+                Sign up as a donor or make a request for blood in just a few clicks.
+            </p>
+            <button className="mt-6 bg-white text-red-600 px-6 py-3 rounded-full font-semibold shadow hover:bg-gray-100 transition duration-300">
                 Get Started
             </button>
-            
         </section>
-    )
-}
+    );
+};
 
 export default Join;
