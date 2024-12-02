5ba86ca5bf77295a7037ceaa2dfc250e89db5a85
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-12-02 20:56:06 +0200

    smooth scroll ensure

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,26 @@ import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import LandingPage from './pages/LandingPage';
 import Join from './components/Join';
-// import BloodType from './components/BloodType';
-// import BloodTypeCompatibility from './components/blood2';
 import BloodTypeTree from './components/BloodTypeTree';
+import ScrollToTop from './components/ScrollToTop'; // Import the new component
+import Features from './pages/Features';
+import { LanguageProvider } from './hooks/LanguageContext';
 
 function App() {
     return (
         <>
+        <LanguageProvider>
             <Navbar />
             <Hero />
             <div className="bg-gray-100 pl-0 md:pl-52">
-                <LandingPage />
-                {/* <BloodType />
-                <BloodTypeCompatibility /> */}
+                <Features />
                 <BloodTypeTree />
                 <Join />
             </div>
             <Footer />
+            <ScrollToTop />
+        </LanguageProvider>
         </>
     );
 }
