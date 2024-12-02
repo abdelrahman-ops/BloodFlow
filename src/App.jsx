087bcdbbe59af5bd2071c46c65e4601cc61aedf1
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-12-02 04:05:55 +0200

    add blood type component in app

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import LandingPage from './pages/LandingPage';
 import Join from './components/Join';
+// import BloodType from './components/BloodType';
+// import BloodTypeCompatibility from './components/blood2';
+import BloodTypeTree from './components/BloodTypeTree';
 
 function App() {
     return (
@@ -12,6 +15,9 @@ function App() {
             <Hero />
             <div className="bg-gray-100 pl-0 md:pl-52">
                 <LandingPage />
+                {/* <BloodType />
+                <BloodTypeCompatibility /> */}
+                <BloodTypeTree />
                 <Join />
             </div>
             <Footer />
