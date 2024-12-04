e9a75e3c5ef04911f8febc7e4595570e02015888
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-12-04 07:22:30 +0200

    add home page with updated components

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,28 @@
+import Navbar from "../components/Navbar.jsx";
+import Hero from "../components/Hero.jsx";
+import Search from "../components/Search.jsx";
+import Stats from "../components/Stats.jsx";
+import Join from "../components/Join.jsx";
+import Features from './Features.jsx'
+import Footer from "../components/Footer.jsx";
+
+const HomePage = () => {
+    return (
+        <>
+            <div className="font-sans">
+                <Navbar />
+                <Hero />
+                <Search />
+                <Stats />
+                <div className="bg-gray-100 pl-0 md:pl-52">
+                    <Features />
+                    <Join />
+                </div>
+                <Footer />
+            </div>
+        </>
+        
+    );
+};
+
+export default HomePage;
