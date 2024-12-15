d3e3978f59998dc0b8a5fc103b6fcd7395f84b69
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-12-15 14:05:31 +0200

    update layeout and styles

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -78,59 +78,63 @@ function Features() {
     const { title, cards } = labels[language];
     
     return (
-        <div className='flex flex-wrap justify-center gap-28 '>
-<div className={`min-h-screen ${language === "ar" ? "text-right" : "text-left"}`}>
-            <section id="features" className="py-6 px-4 sm:px-8">
-                <h3 className="text-3xl font-bold text-center mb-12">
-                    {title}
-                </h3>
-                <div
-                    className={`grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-10 gap-y-6 ${
-                        language === "ar" ? "direction-rtl" : ""
-                    }`}
-                >
-                    {/* Card Components */}
-                    {
-                        cards.map((card, index) => (
-                            <motion.div
-                                key={index}
-                                initial={{ opacity: 0, y: 20 }}
-                                whileInView={{ opacity: 1, y: 0 }}
-                                transition={{ duration: 0.5 }}
-                                className="bg-white shadow-md p-4 sm:p-6 
-                                rounded-tl-2xl rounded-tr-[90px] rounded-b-[100px] mx-auto
-                                flex flex-col justify-between items-center 
-                                overflow-hidden transition-transform 
-                                hover:scale-105 duration-300  relative"
-                                // style={{ width: '250px', height: '400px' }}
-                            >
-                                <div className="text-center block max-w-40 justify-center">
-                                    <h4 className="text-lg sm:text-xl font-bold  text-gray-800  max-h-20">
-                                        {card.title}
-                                    </h4>
-                                    <p className="relative text-sm sm:text-base mt-2 text-gray-600">
-                                        {card.description}
-                                    </p>
-                                </div>
-                                
+        <div className='container mx-auto text-center'>
 
-                                {/* Image container with the icon */}
-                                <div className="relative w-full flex justify-center items-center mt-4">
-                                    <img
-                                        src={card.imgSrc}
-                                        alt={card.altText}
-                                        className="w-24 h-24 sm:w-32 sm:h-32 lg:w-40 lg:h-40 rounded-full hover:scale-110"
-                                    />
-                                    <div className="absolute top-1 right-1 flex items-center justify-center w-8 h-8 bg-red-800 text-white rounded-full shadow-lg hover:bg-red-600 transition duration-300">
-                                        <IoResizeSharp className="w-4 h-4" />
-                                        {/* <AiOutlineEye className="w-4 h-4" /> */}
+        
+        <div className='flex flex-wrap justify-center gap-4'>
+            <div className={`min-h-screen ${language === "ar" ? "text-right" : "text-left"}`}>
+                <section id="features" className="py-6 px-4 sm:px-8">
+                    <h3 className="text-3xl font-bold text-center mb-12">
+                        {title}
+                    </h3>
+                    <div
+                        className={`grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-10 gap-y-6 ${
+                            language === "ar" ? "direction-rtl" : ""
+                        }`}
+                    >
+                        {/* Card Components */}
+                        {
+                            cards.map((card, index) => (
+                                <motion.div
+                                    key={index}
+                                    initial={{ opacity: 0, y: 20 }}
+                                    whileInView={{ opacity: 1, y: 0 }}
+                                    transition={{ duration: 0.5 }}
+                                    className="bg-white shadow-md p-4 sm:p-6 
+                                    rounded-tl-2xl rounded-tr-[90px] rounded-b-[100px] mx-auto
+                                    flex flex-col justify-between items-center 
+                                    overflow-hidden transition-transform 
+                                    hover:scale-105 duration-300  relative"
+                                    // style={{ width: '250px', height: '400px' }}
+                                >
+                                    <div className="text-center block max-w-40 justify-center">
+                                        <h4 className="text-lg sm:text-xl font-bold  text-gray-800  max-h-20">
+                                            {card.title}
+                                        </h4>
+                                        <p className="relative text-sm sm:text-base mt-2 text-gray-600">
+                                            {card.description}
+                                        </p>
                                     </div>
-                                </div>
-                            </motion.div>
-                        ))
-                    }
-                </div>
-            </section>
+                                    
+
+                                    {/* Image container with the icon */}
+                                    <div className="relative w-full flex justify-center items-center mt-4">
+                                        <img
+                                            src={card.imgSrc}
+                                            alt={card.altText}
+                                            className="w-24 h-24 sm:w-32 sm:h-32 lg:w-40 lg:h-40 rounded-full hover:scale-110"
+                                        />
+                                        <div className="absolute top-1 right-1 flex items-center justify-center w-8 h-8 bg-red-800 text-white rounded-full shadow-lg hover:bg-red-600 transition duration-300">
+                                            <IoResizeSharp className="w-4 h-4" />
+                                            {/* <AiOutlineEye className="w-4 h-4" /> */}
+                                        </div>
+                                    </div>
+                                </motion.div>
+                            ))
+                        }
+                    </div>
+                </section>
+            </div>
         </div>
         </div>
         
