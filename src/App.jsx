e81f3d64f3fb563be30d2dd5a82ee1acda66dfcb
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-12-15 14:15:03 +0200

    add routes 4 login and dashboard and handle layeout

--- src/App.jsx
+++ src/App.jsx
@@ -3,64 +3,53 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import BloodRequests from './pages/Request';
 import DonateBlood from './pages/Donate';
-import Dashboard from './pages/Dashboard';
 import AboutUs from './pages/AboutUs';
 import Events from './pages/Events';
 import ContactUs from './pages/Contact';
 
+import DonorDash from './pages/Dashboard/DonorDash';
+import DonorLogin from './pages/Login/DonorLogin'
+import HospitalLogin from './pages/Login/AdminLogin';
+
 import ScrollToTop from './components/ScrollToTop';
+
+import Navbar from './components/Navbar';
+import Footer from './components/Footer'
 
 import { LanguageProvider } from './hooks/LanguageContext';
 
 import './App.css';
+import AdminDash from './pages/Dashboard/AdminDash';
 
-// import Footer from './components/Footer';
-// import Navbar from './components/Navbar';
-// import Hero from './components/Hero';
-// import Join from './components/Join';
-// import BloodTypeTree from './components/BloodTypeTree';
-// import Features from './pages/Features';
-// import Stats from './components/Stats';
 
 
 
 function App() {
     return (
-        <>
-        {/* <LanguageProvider>
-            <Navbar />
-            <div className="relative">
-                <Hero />
-                <div className='relative lg:-mt-16 md:-mt-12 sm:-mt-8 z-30 bg-gray-100 pl-0 md:pl-52'>
-                    <Stats />
-                </div>
-            </div>
+        <div className='bg-gray-100'>
+            <LanguageProvider>
+                <Router>
+                    <Navbar />
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/blood-requests" element={<BloodRequests />} />
+                        <Route path="/donate-blood" element={<DonateBlood />} />
+                        
+                        <Route path="/about-us" element={<AboutUs />} />
+                        <Route path="/events" element={<Events />} />
+                        <Route path="/contact-us" element={<ContactUs />} />
+                        
+                        <Route path="/donor/dashboard" element={<DonorDash />} />
+                        <Route path="/admin/dashboard" element={<AdminDash />} />
+                        <Route path="/hospital/login" element={<HospitalLogin />}/>
+                        <Route path="/user/login" element={<DonorLogin />}/>
+                    </Routes>
+                    <Footer />
+                    <ScrollToTop />
+                </Router>
             
-            <div className="bg-gray-100 pl-0 md:pl-52">
-                <Features />
-                <BloodTypeTree />
-                <Join />
-            </div>
-            <Footer />
-            <ScrollToTop />
-        </LanguageProvider> */}
-
-
-        <LanguageProvider>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/blood-requests" element={<BloodRequests />} />
-                    <Route path="/donate-blood" element={<DonateBlood />} />
-                    <Route path="/dashboard" element={<Dashboard />} />
-                    <Route path="/about-us" element={<AboutUs />} />
-                    <Route path="/events" element={<Events />} />
-                    <Route path="/contact-us" element={<ContactUs />} />
-                </Routes>
-            </Router>
-            <ScrollToTop />
         </LanguageProvider>
-        </>
+        </div>
     );
 }
 
