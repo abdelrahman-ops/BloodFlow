736688c438e90c04172a6efec690295ee15a3d6e
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-12-17 03:39:28 +0200

    add more assits

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -6,6 +6,14 @@ import supportImage from './img/support.jpg';
 import doctorsImage from './img/doctors.jpg';
 import donorImage from './img/donor.jpg';
 import hero from './img/hero.jpg';
+import accident from './img/accident.png';
+import child from './img/child.jpg';
+import surgery1 from './img/surgery.png';
+import surgery2 from './img/surgery.jpeg';
+import support1 from './img/support1.jpg';
+import hdonor from './img/Hdonor.jpg';
+import unit from './img/bloodunit.jpg';
+
 
 import arrow1Image from './icons/fromarrow.png';
 import arrow2Image from './icons/toarrow.png';
@@ -25,6 +33,13 @@ const assets = {
         doctors: doctorsImage,
         donor: donorImage,
         hero: hero,
+        accident: accident,
+        child: child,
+        surgery1: surgery1,
+        surgery2: surgery2,
+        support1: support1,
+        hdonor:hdonor,
+        unit: unit,
     },
     arrows: {
         down: arrow1Image,
