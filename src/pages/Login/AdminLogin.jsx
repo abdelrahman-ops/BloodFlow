b240c724552345d8c8cd0378fcf8cb7c9b6ae68e
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-12-17 03:43:04 +0200

    update styles

--- src/pages/Login/AdminLogin.jsx
+++ src/pages/Login/AdminLogin.jsx
@@ -2,7 +2,7 @@ import LoginForm from "../../components/LoginForm";
 
 const HospitalLogin = () => {
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-gray-700 via-gray-900 to-black">
+        <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-gray-700 via-gray-900 to-[#0D1321]">
             <div className="p-6 max-w-md w-full">
                 <LoginForm role="hospital" heading="Hospital Admin Login" />
             </div>
