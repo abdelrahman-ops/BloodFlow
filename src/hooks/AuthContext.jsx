45e597d5bd0f0a06f51a73cca6d612c83d0d7d8f
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-12-19 23:36:24 +0200

    update authentication with js cookie

--- src/hooks/AuthContext.jsx
+++ src/hooks/AuthContext.jsx
@@ -1,46 +1,35 @@
-/* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
-import  { createContext, useContext, useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-
+/* eslint-disable react-refresh/only-export-components */
+import { createContext, useContext, useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
 
 const AuthContext = createContext();
 
+export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
-    const navigate = useNavigate();
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    
-    const login = (userData) => {
-        setUser(userData);
-        
-        localStorage.setItem("user", JSON.stringify(userData));
-    };
-
-    
-    const logout = () => {
-        setUser(null);
-        localStorage.removeItem("user");
-        navigate("/");
-    };
-
-    
     useEffect(() => {
-        const savedUser = localStorage.getItem("user");
-        if (savedUser) {
-            setUser(JSON.parse(savedUser));
+        const token = Cookies.get("token");
+        if (token) {
+            setIsAuthenticated(true);
         }
     }, []);
 
+    const login = () => {
+        setIsAuthenticated(true);
+    };
+
+    const logout = () => {
+        Cookies.remove("token");
+        localStorage.removeItem('userData');
+        setIsAuthenticated(false);
+    };
+
     return (
-        <AuthContext.Provider value={{ user, login, logout }}>
+        <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
             {children}
         </AuthContext.Provider>
     );
-};
-
-
-export const useAuth = () => {
-    return useContext(AuthContext);
 };
